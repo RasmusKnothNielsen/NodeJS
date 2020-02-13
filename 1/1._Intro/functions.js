4b3e748a00599f00b35f98a961030fbdb771dd57
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-02-13 09:28:35 +0100

    Added functions

---
+++ 1/1._Intro/functions.js
@@ -0,0 +1,46 @@
+// Functions
+
+// You can use a function, even though it's defined later in the code. 
+// It is not procedural
+// It works because of hoisting 
+//(The program parses everything, and puts functions etc in the in the memory space for later consumption)
+console.log(addition(1,1));
+
+// Defining a function in JavaScript
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+//console.log(addition(1,4));
+
+// This doesn't work for var
+// test = 10;
+// var test;
+// Since it when using var, it creates a global variable.
+// Use let and const insted.
+
+function pokeMe() {
+    console.log("Meow");
+}
+
+
+function approachSomeone(someoneToPoke) {
+    console.log("tip tap tip tap");
+    someoneToPoke();
+}
+
+approachSomeone(pokeMe);
+
+const introduce = (name) => {
+    console.log("Hello my name is", name);
+}
+
+introduce("Rasmus");
+
+const prepareIntroduction = (introducerFunction, name) => {
+    console.log("Hmmhmhmhhmmhhm");
+    introducerFunction(name);
+}
+
+prepareIntroduction(introduce, "Rasmus");
+
