53e004c61e055913f37583ce3a84288c2d6c9d8f
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-02-13 11:21:21 +0100

    Updated app

--- 1/2._First_API/app.js
+++ 1/2._First_API/app.js
@@ -1,6 +1,40 @@
+const express = require("express");
 
-var express = require("express");
-var app = express();
+const app = express();
+
+// The above can be onelined
+//const app = require("express")();
+
+// Creating a get request for the webserver to use
+// First argument is the path
+// Second argument is a callback function that takes a request and provides a response
+app.get("/", (req, res) => {
+    res.send({message: "Hello there"});
+});
+
+// Define something on the path /aboutMe that returns a JSON
+// First argument is the path
+// Second argument is a callback function 
+app.get("/aboutMe", (req, res) => {
+    res.send({message: "About me"});
+});
+
+// Define something on the path /aboutThisWebsite that returns a JSON representation of the website
+app.get("/aboutThisWebsite", (req, res) => {
+    res.send({
+        about: "This website contains alot of things!",
+        api: {
+            aboutme: "/aboutMe contains information about me",
+            index: "/ contains the root of the site"
+        }
+    });
+});
 
 
-app.listen(3000);
+// Start the webserver on port 8686
+app.listen(8686, error => {
+    if(error) {
+        console.log(error.message);
+    }
+    console.log("Server started on port:", 8686);
+});
