bf92a18b9a1197e98c98229194fd42b983d0530a
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-03-05 14:05:12 +0100

    Updated repo

--- 1/Mandatory_1/app.js
+++ 1/Mandatory_1/app.js
@@ -4,9 +4,15 @@ const portNumber = 8686
 
 const app = express();
 
+// GET HTTP Request for root
+app.get("/", (req, res) => {
+    return res.sendFile(__dirname + "/public/index.html");
+})
+
+// Start the server
 app.listen(portNumber, error => {
     if (error) {
         console.log(error);
     }
-    console.log("Server started up on port: " + portNumber);
+    console.log("Server started up on port: ", portNumber);
 })
