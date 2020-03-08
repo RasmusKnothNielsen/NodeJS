26de1bd19f184a5c8701eaf84b2e23cbbe45372b
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-03-08 17:51:27 +0100

    Starting building the HTML pages

--- 1/Mandatory_1/app.js
+++ 1/Mandatory_1/app.js
@@ -9,6 +9,21 @@ app.get("/", (req, res) => {
     return res.sendFile(__dirname + "/public/index.html");
 })
 
+// GET Request for NodeJS notes
+app.get("/nodejs", (req, res) => {
+    return res.sendFile(__dirname + "/public/NodeJS.html");
+})
+
+// Get Request for JavaScript and JQuery
+app.get("/javascriptandjquery", (req, res) => {
+    return res.sendFile(__dirname + "/public/Javascript_JQuery.html");
+})
+
+// Get Request for Express
+app.get("/express", (req, res) => {
+    return res.sendFile(__dirname + "/public/Express.html");
+})
+
 // Start the server
 app.listen(portNumber, error => {
     if (error) {
