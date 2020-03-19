6a6c66a0b6811512877e7b14c03320a350aea025
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-03-19 11:37:20 +0100

    Added notes and refactored code

--- 1/5._Keatube/Notes.md
+++ 1/5._Keatube/Notes.md
@@ -14,3 +14,15 @@ Add it to package.json in the top as
 
 "start" script starts the app,
 "start-dev" runs the app in developer mode (DO NOT USE IN PRODUCTION)
+
+
+## Server Side Rendering
+SSR Server Side Rendering
+Used to read html files into memory, so we can concatonate them in the correct way
+and then send the files as strings, so the browser can interpret them.
+This way, we can combine our header, footer and "main view"
+
+## How to check if JQuery is loaded
+Do the following in a .js file
+> console.log($)
+If it displays a function, JQuery is imported correctly
--- 1/5._Keatube/app.js
+++ 1/5._Keatube/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// Import the fs to be able to interact with the filesystem
+const fs = require('fs');
+
 // Enable express to parse json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,16 +20,21 @@ app.use(express.static('videos'));
 // If undefined, start on 8686, else start on the provided portnumber
 const port = process.env.PORT ? process.env.PORT : 8686;
 
-
-// Get Request for the player page
-app.get('/player/:videoid', (req, res) => {
-	// console.log(req.params.videoid);
-	return res.sendFile(__dirname + '/public/player/player.html');
-});
+// Load the navbar, footer and frontpage
+// Using readFileSync, blocks the app from going on, before the file is read
+const navbarPage = fs.readFileSync(__dirname + '/public/navbar/navbar.html', 'utf-8');
+const footerPage = fs.readFileSync(__dirname + '/public/footer/footer.html', 'utf-8');
+const frontpagePage = fs.readFileSync(__dirname + '/public/frontpage/frontpage.html', 'utf-8');
+const playerPage = fs.readFileSync(__dirname + '/public/player/player.html', 'utf-8');
 
 // Get Request for front page
 app.get('/', (req, res) => {
-	return res.sendFile(__dirname + '/public/frontpage/frontpage.html');
+	return res.send(navbarPage + frontpagePage + footerPage);
+});
+
+// Get Request for the player page
+app.get('/player/:videoid', (req, res) => {
+	return res.send(navbarPage + playerPage + footerPage);
 });
 
 
