5a1d2039cc558f25de84dac31dacc9bdd6694c12
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-05-22 15:04:23 +0200

    Minor changes

--- 1/8._Auth/routes/auth.js
+++ 1/8._Auth/routes/auth.js
@@ -179,21 +179,26 @@ router.post('/resetpassword', async (req, res) => {
 
 // route for resetting password
 router.post('/passwordreset', async (req, res) => {
+
+    // Capture the information from the form
     const username = req.body.username;
     const token = req.body.token;
     const password = req.body.password;
     const passwordRepeat = req.body.passwordRepeat;
-    console.log(req.body.username);
-    console.log(req.body.token);
 
     // Check if the token is the one saved in the dictionary
     if (resetPasswordDict[username] = token) {
+
         // If it is, check if password and passwordRepeat match
         if (password != undefined && password == passwordRepeat) {
+
             // if they do, hash password and save it to the database
             const hashedPassword = await bcrypt.hash(password, saltRounds);
-            console.log("New hashed password = ", hashedPassword)
+            console.log("New hashed password = ", hashedPassword);
             const userUpdated = await User.query().where('username', '=', username).update({ password: hashedPassword});
+
+            // Remember to remove the entry in our dictionary
+            delete resetPasswordDict[username];
             return res.status(200).redirect('/login');
         }
         // Passwords does not match
@@ -201,38 +206,11 @@ router.post('/passwordreset', async (req, res) => {
             return res.status(401).send({response: "Passwords must match eachother"});
         }
     }
+    // User provided an invalid token
     else {
         return res.status(401).send({response: "Invalid token entered"});
     }
 })
-
-/*
-router.get('/secure', async (req, res) => {
-    // If we have a session, a UUID and is authenticated
-    console.log("Going to /secure")
-    if (req.session && req.session.user != undefined && req.session.authenticated == true) {
-        // Check if UUID is in DB
-        console.log("Checking if UUID is in db")
-        console.log(req.session.user)
-        
-        const result = await User.query().select().where({'uuid': req.session.user}).limit(1);
-        // If UUID found
-        if (result.length > 0) {
-            // Send the secure page
-            const securePage = fs.readFileSync(__dirname + '/public/securepage.html', 'utf-8');
-            console.log("This is the UUID of the user, that is logged in:", req.session.user);
-            return res.send(navbarPage + securePage + footerPage);
-        }
-        // UUID not found
-        else {
-            return res.status(401).redirect('/login');
-        }
-    }
-    else {
-        return res.status(401).redirect('/login');
-    }
-})
-*/
 
 router.get('/logout', (req, res) => {
     console.log(req.session);
