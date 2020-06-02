9705c6f95816e40c2ef68b9926a7738a4aeb8e9e
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-06-02 15:41:17 +0200

    Started Exam Project

---
+++ 1/Exam_Project/TODO.md
@@ -0,0 +1,23 @@
+- Get Auth to work
+- Add sockets at the chatpage
+- Add chatpage
+- Add video info to Database instead of file
+- Implement Sweetalert pop ups
+
+
+
+Double Check HARD REQUIREMENTS:  
+Backend  
+- Express
+- Database
+- ORM / ODM
+
+Frontend  
+- Plain HTML
+- AJAX / Fetch
+- Sockets
+
+SOFT REQUIREMENTS:  
+- Clean Codebase
+- Style website
+- Host Application on AWS or the like
---
+++ 1/Exam_Project/app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+
+// Import the fs to be able to interact with the filesystem
+const fs = require('fs');
+
+// Enable express to parse json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Make NodeJS able to serve the files from /public and /videos.
+app.use(express.static('public'));
+app.use(express.static('videos'));
+
+// If undefined, start on 8686, else start on the provided portnumber
+const port = process.env.PORT ? process.env.PORT : 8686;
+
+// Load the navbar, footer and frontpage
+// Using readFileSync, blocks the app from going on, before the file is read
+const navbarPage = fs.readFileSync(__dirname + '/public/navbar/navbar.html', 'utf-8');
+const footerPage = fs.readFileSync(__dirname + '/public/footer/footer.html', 'utf-8');
+
+const frontpagePage = fs.readFileSync(__dirname + '/public/frontpage/frontpage.html', 'utf-8');
+const playerPage = fs.readFileSync(__dirname + '/public/player/player.html', 'utf-8');
+const uploadPage = fs.readFileSync(__dirname + '/public/upload/upload.html', 'utf-8');
+
+// Get Request for front page
+app.get('/', (req, res) => {
+	return res.send(navbarPage + frontpagePage + footerPage);
+});
+
+// Get Request for the player page
+app.get('/player/:videoid', (req, res) => {
+	return res.send(navbarPage + playerPage + footerPage);
+});
+
+// Upload videos
+app.get('/upload', (req, res) => {
+	return res.send(navbarPage + uploadPage + footerPage);
+});
+
+
+// How to import routes and use them from another file
+// Import routes
+const videosRoute = require('./routes/videos');
+// Setup routes
+app.use(videosRoute);
+
+
+// Start the server on the provided port
+app.listen(port, error => {
+	if (error) {
+		console.log(error.log);
+	}
+	console.log('The server has started on port', port);
+},
+);
+
